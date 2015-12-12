7660f05f0fb46c24fb526d4d2ef81969bb21dd01
Viktor Pekar <vikpek@gmail.com>
2015-12-12 10:09:11 +0100

    Added mutators and basic moving functionality

---
+++ LD34Project/Assets/MutatorController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MutatorController : MonoBehaviour {
+
+	Vector3 maximumSize;
+	Vector3 minimumSize;
+
+	bool growing = true;
+	bool shrinking = false;
+
+	void Start () {
+		minimumSize = new Vector3 (
+			PercentageFor (transform.localScale.x, Random.Range(60, 99)), 
+			PercentageFor (transform.localScale.y, Random.Range(60, 99)), 
+			PercentageFor (transform.localScale.z, Random.Range(60, 99)));
+
+		maximumSize = new Vector3 (
+			PercentageFor (transform.localScale.x, Random.Range(101, 160)), 
+			PercentageFor (transform.localScale.y, Random.Range(101, 160)), 
+			PercentageFor (transform.localScale.z, Random.Range(101, 160)));
+	}
+
+	void Update () {
+		print (Vector3.Distance(transform.localScale,maximumSize));
+		if (growing == true && shrinking == false) {
+			transform.localScale = Vector3.Lerp (transform.localScale, maximumSize, Random.Range(0.05f, 0.2f));
+			if (Vector3.Distance(transform.localScale,maximumSize) < .5) {
+				growing = !growing;
+				shrinking = !shrinking;
+			}
+		}
+
+		if (growing == false && shrinking == true) {
+			transform.localScale = Vector3.Lerp (transform.localScale, minimumSize, Random.Range(0.05f, 0.2f));
+			if (Vector3.Distance(transform.localScale,minimumSize) < .5) {
+				growing = !growing;
+				shrinking = !shrinking;
+			}
+		}
+			
+	}	
+
+
+	float PercentageFor(float value, float percentage){
+		float onepercent = value / 100;
+		return  onepercent * percentage;
+	}
+}
---
+++ LD34Project/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	bool runningDirectionRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessKeyInput ();
+		Run ();
+	}
+
+	void ProcessKeyInput(){
+		if (Input.GetKey (KeyCode.V)) {
+			transform.position = transform.position + Vector3.up * 0.1f;
+		}
+		if (Input.GetKey (KeyCode.B)) {
+			runningDirectionRight = !runningDirectionRight;
+		}
+	}
+
+	void Run(){
+		if (runningDirectionRight) {
+			transform.position = transform.position + (Vector3.right*0.1f);
+		}
+		if (!runningDirectionRight) {
+			transform.position = transform.position + (Vector3.left*0.1f);
+		}
+	}
+}
